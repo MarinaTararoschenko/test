468980c5dab3d0aa8599597af35982d972f8fa5f
Marina Tararoshenko <tararoshenkom@gmail.com>
2022-12-19 17:03:43 +0300

    feat: added dynamic-list

--- src/app/home/dynamic-list/dynamic-list.component.ts
+++ src/app/home/dynamic-list/dynamic-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Renderer2 } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TestDataService } from 'src/app/shared/services/data.service';
 
 @Component({
@@ -14,22 +15,41 @@ export class DynamicListComponent implements OnInit {
     public textButton: string = 'Add';
     public isReadonly: boolean = true;
 
+    public form: FormGroup;
+
     constructor(
         public dataSrv: TestDataService,
-        public renderer: Renderer2
+        public renderer: Renderer2,
+        private fb: FormBuilder
     ) {
-        this.dataSrv.getJSON().subscribe(item => this.data = item);
+        this.form = this.fb.group({
+            newName: new FormControl('', [Validators.minLength(3), Validators.required]),
+            names: this.fb.array([])
+        });
     }
 
     ngOnInit(): void {
+        this.dataSrv.getJSON().subscribe(item => {
+            this.data = item;
+            for (let i = 0; i < this.data.length; i++) {
+                this.names.push(new FormControl(this.data[i].name, [Validators.minLength(3), Validators.required]));
+            }
+        });
     }
 
-    public addItem(input: any): void {
+    get names(): FormArray {
+        return this.form.get('names') as FormArray;
+    }
+
+    get newName(): FormControl { return this.form.get('newName')! as FormControl }
+
+    public addItem(): void {
         this.data.push({
             id: this.data.length,
-            name: input.value,
-            state: 'disactive'
+            name: this.form.controls['newName'].value
         });
+        this.names.push(new FormControl(this.newName.value, Validators.minLength(3)));
+        this.newName.reset();
     }
 
     public removeItem(ev: any, index: number): void {
@@ -39,6 +59,7 @@ export class DynamicListComponent implements OnInit {
         setTimeout(() => {
             this.currentSwipe = null;
             this.data.splice(index, 1);
+            this.names.removeAt(index);
         }, 300);
     }
 
@@ -47,13 +68,19 @@ export class DynamicListComponent implements OnInit {
         this.currentSwipe = this.currentSwipe === index ? null : index;
     }
 
-    public changeItem(index: number, input: HTMLInputElement): void {
+    public toEdit(index: number, input: HTMLInputElement): void {
         input.focus();
+        input.value = this.data[index].name;
         this.currentEdit = index;
         this.currentSwipe = null;
     }
 
     public changeName(index: number, input: HTMLInputElement): void {
+        this.currentEdit = null;
+        this.data[index].name = input.value;
+    }
+
+    public save(index: number, input: HTMLInputElement): void {
         this.currentEdit = null;
         this.data[index].name = input.value;
     }
